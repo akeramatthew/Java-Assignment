9b22fe294b2f2eaa24880e7778084911d29e0d43
akeramatthew <akeramatthew@outlook.com>
2017-05-13 01:26:21 +0800

    Add files via upload

---
+++ Assessment.java
@@ -0,0 +1,19 @@
+public class Assessment {
+    public static void main(String[] args) {
+        System.out.println("Welcome to code2flow");
+        boolean inDoubt = true;
+        boolean Ready = true;
+        boolean Ready1 = true;
+        if (inDoubt) {
+            System.out.println("Press help");
+            while (Ready == false) {
+                System.out.println("Read help");
+            }
+            if (Ready1) {
+                System.out.println("My name is Chris");
+
+            }
+        }
+        System.out.println("Improve your workflow");
+    }
+}
